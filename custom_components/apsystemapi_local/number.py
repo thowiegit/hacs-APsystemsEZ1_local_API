f7e67cedf4316da6585de1e1fb8cfa6ba7fdbe88
Andy <145973686+AndyNew2@users.noreply.github.com>
2026-03-15 21:20:55 +0100

    Added proper error handling and prevented raise conditions
    added further loggings as well

--- custom_components/apsystemapi_local/number.py
+++ custom_components/apsystemapi_local/number.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from aiohttp import ClientConnectorError
+import asyncio
 
 from homeassistant.components.number import NumberDeviceClass, NumberEntity, NumberMode
 from homeassistant.const import UnitOfPower
@@ -13,6 +14,8 @@ from homeassistant.helpers.typing import DiscoveryInfoType
 from .coordinator import ApSystemsConfigEntry, ApSystemsData
 from .entity import ApSystemsEntity
 
+import logging
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -41,20 +44,52 @@ class ApSystemsMaxOutputNumber(ApSystemsEntity, NumberEntity):
         """Initialize the sensor."""
         super().__init__(data)
         self._api = data.coordinator.api
+        self._coordinator = data.coordinator
         self._attr_unique_id = f"{data.device_id}_output_limit"
         self._attr_native_max_value = data.coordinator.api.max_power
         self._attr_native_min_value = data.coordinator.api.min_power
 
     async def async_update(self) -> None:
         """Set the state with the value fetched from the inverter."""
+
+        counter: int = 0
+        while self._coordinator.currently_running:
+            await asyncio.sleep(2)  # Locking for poor people, but better than nothing...
+            counter += 1  # usually we could stop updating, however maxout is rearly updated, therefore give it a little retry ..
+            if counter > 3:  # After 8 seconds of waiting, give up
+                _LOGGER.debug("Update already running, skipping max_power...")
+                return # Skip update if coordinator is currently running an update
+
         try:
+            self._coordinator.currently_running = True  # Set coordinator to running state to prevent concurrent updates
             status = await self._api.get_max_power()
-        except (TimeoutError, ClientConnectorError):
+        except:
+            _LOGGER.debug("Cannot update max power. Retry next cycle...")
             self._attr_available = False
         else:
             self._attr_available = True
             self._attr_native_value = status
+        finally:
+            self._coordinator.currently_running = False  # Reset running state on error
 
     async def async_set_native_value(self, value: float) -> None:
         """Set the desired output power."""
-        self._attr_native_value = await self._api.set_max_power(int(value))
+        counter: int = 0
+
+        while self._coordinator.currently_running:
+            await asyncio.sleep(1)  # Locking for poor people, but better than nothing...
+            counter += 1
+            if counter > 15:  # After 15 seconds of waiting, give up and raise an error
+                _LOGGER.warning("Timeout while waiting for coordinator to be free. Aborting setting max power.")
+                counter = 0
+                return
+
+        try:
+            self._coordinator.currently_running = True  # Set coordinator to running state to prevent concurrent updates
+            self._attr_native_value = await self._api.set_max_power(int(value))
+            self._attr_available = True
+        except:
+            _LOGGER.error("Error while setting max power.")
+            self._attr_available = False
+        finally:
+            self._coordinator.currently_running = False  # Reset running state on error
