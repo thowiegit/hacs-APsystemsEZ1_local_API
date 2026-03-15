208cff13f8db045152797dd32ae58ce82b4c9607
Andy <145973686+AndyNew2@users.noreply.github.com>
2026-03-15 21:21:21 +0100

    Enhance switch control with retry and error handling
    Added error handling and retry logic for device power status updates.

--- custom_components/apsystemapi_local/switch.py
+++ custom_components/apsystemapi_local/switch.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from typing import Any
+import asyncio
 
 from aiohttp.client_exceptions import ClientConnectionError
 from .APsystemsEZ1 import InverterReturnedError
@@ -14,6 +15,8 @@ from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 from .coordinator import ApSystemsConfigEntry, ApSystemsData
 from .entity import ApSystemsEntity
 
+import logging
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -35,24 +38,73 @@ class ApSystemsInverterSwitch(ApSystemsEntity, SwitchEntity):
         """Initialize the switch."""
         super().__init__(data)
         self._api = data.coordinator.api
+        self._coordinator = data.coordinator
         self._attr_unique_id = f"{data.device_id}_inverter_status"
         if data.coordinator.battery_system:
             self._attr_available = False
 
     async def async_update(self) -> None:
         """Update switch status and availability."""
+        counter: int = 0
+        while self._coordinator.currently_running:
+            await asyncio.sleep(2)  # Locking for poor people, but better than nothing...
+            counter += 1  # usually we could stop updating, however switch status is rearly updated, therefore give it a little retry ..
+            if counter > 3:  # After 8 seconds of waiting, give up
+                _LOGGER.debug("Update already running, skipping switch...")
+                return # Skip update if coordinator is currently running an update
+
         try:
+            self._coordinator.currently_running = True  # Set coordinator to running state to prevent concurrent updates
             status = await self._api.get_device_power_status()
-        except (TimeoutError, ClientConnectionError, InverterReturnedError):
+        except:
             self._attr_available = False
+            _LOGGER.info("Cannot update switch status. Retry next cycle...")
         else:
             self._attr_available = True
             self._attr_is_on = status
+        finally:
+            self._coordinator.currently_running = False  # Reset running state on error
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn the switch on."""
-        await self._api.set_device_power_status(True)
+        counter: int = 0
+        while self._coordinator.currently_running:
+            await asyncio.sleep(1)  # Locking for poor people, but better than nothing...
+            counter += 1
+            if counter > 20:  # After 20 seconds of waiting, give up and raise an error
+                _LOGGER.warning("Timeout while waiting for coordinator to be free. Aborting setting power on.")
+                counter = 0
+                return
+
+        try:
+            self._coordinator.currently_running = True  # Set coordinator to running state to prevent concurrent updates
+            await self._api.set_device_power_status(True)
+            self._attr_available = True
+            self._attr_is_on = True
+        except:
+            _LOGGER.error("Error while setting power on.")
+        finally:
+            self._coordinator.currently_running = False  # Reset running state on error
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn the switch off."""
-        await self._api.set_device_power_status(False)
+
+        counter: int = 0
+        while self._coordinator.currently_running:
+            await asyncio.sleep(1)  # Locking for poor people, but better than nothing...
+            counter += 1
+            if counter > 20:  # After 20 seconds of waiting, give up and raise an error
+                _LOGGER.warning("Timeout while waiting for coordinator to be free. Aborting setting power off.")
+                counter = 0
+                return
+
+        try:
+            self._coordinator.currently_running = True  # Set coordinator to running state to prevent concurrent updates
+            await self._api.set_device_power_status(False)
+            self._attr_available = True
+            self._attr_is_on = False
+        except:
+            _LOGGER.error("Error while setting power off.")
+            self._attr_available = False
+        finally:
+            self._coordinator.currently_running = False  # Reset running state on error
