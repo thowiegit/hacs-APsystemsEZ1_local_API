bdcc88c99ddadeb1f3f090c12e4b58cbe0b3b6a7
Andy <145973686+AndyNew2@users.noreply.github.com>
2026-03-28 19:01:37 +0100

    Update switch.py
    Added support for slow coordinator

--- custom_components/apsystemapi_local/switch.py
+++ custom_components/apsystemapi_local/switch.py
@@ -11,6 +11,9 @@ from .APsystemsEZ1 import InverterReturnedError
 from homeassistant.components.switch import SwitchDeviceClass, SwitchEntity
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+)
 
 from .coordinator import ApSystemsConfigEntry, ApSystemsData
 from .entity import ApSystemsEntity
@@ -25,10 +28,12 @@ async def async_setup_entry(
 ) -> None:
     """Set up the switch platform."""
 
-    add_entities([ApSystemsInverterSwitch(config_entry.runtime_data)], True)
+    powerSwitch = ApSystemsInverterSwitch(config_entry.runtime_data)
+    config_entry.runtime_data.slow_coordinator.setPowerSwitchEntity(powerSwitch)
+    add_entities([powerSwitch], True)
 
 
-class ApSystemsInverterSwitch(ApSystemsEntity, SwitchEntity):
+class ApSystemsInverterSwitch(CoordinatorEntity, ApSystemsEntity, SwitchEntity):
     """The switch class for APSystems switches."""
 
     _attr_device_class = SwitchDeviceClass.SWITCH
@@ -36,7 +41,12 @@ class ApSystemsInverterSwitch(ApSystemsEntity, SwitchEntity):
 
     def __init__(self, data: ApSystemsData) -> None:
         """Initialize the switch."""
-        super().__init__(data)
+        CoordinatorEntity.__init__(self,
+            coordinator=data.slow_coordinator
+        )
+        ApSystemsEntity.__init__(self,
+            data=data
+        )
         self._api = data.coordinator.api
         self._coordinator = data.coordinator
         self._attr_unique_id = f"{data.device_id}_inverter_status"
@@ -46,25 +56,16 @@ class ApSystemsInverterSwitch(ApSystemsEntity, SwitchEntity):
     async def async_update(self) -> None:
         """Update switch status and availability."""
         _LOGGER.debug("Updating inverter switch...")
-        counter: int = 0
-        while self._coordinator.currently_running:
-            await asyncio.sleep(0.9)  # Locking for poor people, but better than nothing...
-            counter += 1  # usually we could stop updating, however switch status is rearly updated, therefore give it a little retry ..
-            if counter > 4:  # After 3.6 seconds of waiting, give up
-                _LOGGER.debug("Update already running, skipping switch...")
-                return # Skip update if coordinator is currently running an update
+        # locking is done by coordinator, nothing to do here
 
         try:
-            self._coordinator.currently_running = True  # Set coordinator to running state to prevent concurrent updates
             status = await self._api.get_device_power_status()
         except:
             self._attr_available = False
-            _LOGGER.info("Cannot update switch status. Retry next cycle...")
+            _LOGGER.info("Exception while updating switch status. Retry next cycle...")
         else:
             self._attr_available = True
             self._attr_is_on = status
-        finally:
-            self._coordinator.currently_running = False  # Reset running state on error
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn the switch on."""
@@ -82,6 +83,7 @@ class ApSystemsInverterSwitch(ApSystemsEntity, SwitchEntity):
             await self._api.set_device_power_status(True)
             self._attr_available = True
             self._attr_is_on = True
+            self.async_write_ha_state()
         except:
             _LOGGER.error("Error while setting power on.")
         finally:
@@ -104,6 +106,7 @@ class ApSystemsInverterSwitch(ApSystemsEntity, SwitchEntity):
             await self._api.set_device_power_status(False)
             self._attr_available = True
             self._attr_is_on = False
+            self.async_write_ha_state()
         except:
             _LOGGER.error("Error while setting power off.")
             self._attr_available = False
