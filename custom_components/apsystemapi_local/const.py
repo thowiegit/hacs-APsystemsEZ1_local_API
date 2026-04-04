2f6979bdbfdd57797386f3ab5722af07d14f7c50
Andy <145973686+AndyNew2@users.noreply.github.com>
2026-04-04 14:38:55 +0200

    Add daily debounce constants for P1 and P2

--- custom_components/apsystemapi_local/const.py
+++ custom_components/apsystemapi_local/const.py
@@ -8,3 +8,5 @@ DEFAULT_PORT = 8050
 UPDATE_INTERVAL = "update_interval"
 BASE_PRODUCED_P1 = "base_produced_p1"
 BASE_PRODUCED_P2 = "base_produced_p2"
+DAILY_DEBOUNCE_P1 = "daily_debounce_p1"
+DAILY_DEBOUNCE_P2 = "daily_debounce_p2"
