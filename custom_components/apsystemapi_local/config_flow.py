ef3ba1aa14c02bb238b521a26e1280c07dc2c4ef
Andy <145973686+AndyNew2@users.noreply.github.com>
2026-04-04 14:38:35 +0200

    Enhance config flow with daily debounce parameters
    Added DAILY_DEBOUNCE_P1 and DAILY_DEBOUNCE_P2  for storage handling only. Config is not changed

--- custom_components/apsystemapi_local/config_flow.py
+++ custom_components/apsystemapi_local/config_flow.py
@@ -13,9 +13,18 @@ from homeassistant.const import CONF_IP_ADDRESS, CONF_PORT
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from .const import DEFAULT_PORT, DOMAIN, UPDATE_INTERVAL, BASE_PRODUCED_P1, BASE_PRODUCED_P2
+from .const import (
+    DEFAULT_PORT,
+    DOMAIN,
+    UPDATE_INTERVAL,
+    BASE_PRODUCED_P1,
+    BASE_PRODUCED_P2,
+    DAILY_DEBOUNCE_P1,
+    DAILY_DEBOUNCE_P2,
+)
 
 import logging
+
 _LOGGER = logging.getLogger(__name__)
 
 DATA_SCHEMA = vol.Schema(
@@ -23,8 +32,10 @@ DATA_SCHEMA = vol.Schema(
         vol.Required(CONF_IP_ADDRESS): cv.string,
         vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
         vol.Optional(UPDATE_INTERVAL, default=15): int,
-        vol.Optional(BASE_PRODUCED_P1): cv.string,   # due to a bug, we need to read here a string and convert later
-        vol.Optional(BASE_PRODUCED_P2): cv.string
+        vol.Optional(
+            BASE_PRODUCED_P1
+        ): cv.string,  # due to a bug, we need to read here a string and convert later
+        vol.Optional(BASE_PRODUCED_P2): cv.string,
     }
 )
 
@@ -49,31 +60,39 @@ class APsystemsLocalAPIFlow(ConfigFlow, domain=DOMAIN):
             )
             try:
                 device_info = await api.get_device_info()
-            except (TimeoutError, ClientConnectionError):
+            except TimeoutError, ClientConnectionError:
                 errors["base"] = "cannot_connect"
             else:
                 await self.async_set_unique_id(device_info.deviceId)
                 self._abort_if_unique_id_configured()
 
                 # from info: Store is now a Generic class: self._store = Store[dict[str, int]](hass, STORAGE_VERSION, STORAGE_KEY)
-                _store = Store[dict[str, float]](self.hass, 1, f"{DOMAIN}_storage_{device_info.deviceId}")
+                _store = Store[dict[str, float]](
+                    self.hass, 1, f"{DOMAIN}_storage_{device_info.deviceId}"
+                )
                 _sData = await _store.async_load()
                 try:
                     if _sData is not None:
-                        _sData =  {
+                        _sData = {
                             # use stored values if no user input, however user input always overwrites stored values
                             BASE_PRODUCED_P1: float(user_input.get(BASE_PRODUCED_P1, _sData.get(BASE_PRODUCED_P1, 0))),
-                            BASE_PRODUCED_P2: float(user_input.get(BASE_PRODUCED_P2, _sData.get(BASE_PRODUCED_P2, 0)))
+                            BASE_PRODUCED_P2: float(user_input.get(BASE_PRODUCED_P2, _sData.get(BASE_PRODUCED_P2, 0))),
+                            DAILY_DEBOUNCE_P1: float(_sData.get(DAILY_DEBOUNCE_P1, 0)),
+                            DAILY_DEBOUNCE_P2: float(_sData.get(DAILY_DEBOUNCE_P2, 0)),
                         }
                     else:
-                        _sData =  {
+                        _sData = {
                             BASE_PRODUCED_P1: float(user_input.get(BASE_PRODUCED_P1, 0)),
-                            BASE_PRODUCED_P2: float(user_input.get(BASE_PRODUCED_P2, 0))
+                            BASE_PRODUCED_P2: float(user_input.get(BASE_PRODUCED_P2, 0)),
                         }
                     await _store.async_save(_sData)
                 except ValueError:
-                    errors[BASE_PRODUCED_P1] = "invalid value for base produced power. Must be a float number in kWh."
-                    errors[BASE_PRODUCED_P2] = "invalid value for base produced power. Must be a float number in kWh."
+                    errors[BASE_PRODUCED_P1] = (
+                        "invalid value for base produced power. Must be a float number in kWh."
+                    )
+                    errors[BASE_PRODUCED_P2] = (
+                        "invalid value for base produced power. Must be a float number in kWh."
+                    )
                 # await session.close() # seems HA do not like that
                 else:
                     return self.async_create_entry(
@@ -86,7 +105,6 @@ class APsystemsLocalAPIFlow(ConfigFlow, domain=DOMAIN):
             data_schema=DATA_SCHEMA,
             errors=errors,
         )
-
 
     async def async_step_reconfigure(
         self, user_input: dict[str, Any] | None = None
@@ -105,42 +123,50 @@ class APsystemsLocalAPIFlow(ConfigFlow, domain=DOMAIN):
             )
             try:
                 device_info = await api.get_device_info()
-            except (TimeoutError, ClientConnectionError):
+            except TimeoutError, ClientConnectionError:
                 errors["base"] = "cannot_connect"
             else:
                 # await self.async_set_unique_id(device_info.deviceId)
 
                 # from info: Store is now a Generic class: self._store = Store[dict[str, int]](hass, STORAGE_VERSION, STORAGE_KEY)
-                _store = Store[dict[str, float]](self.hass, 1, f"{DOMAIN}_storage_{device_info.deviceId}")
+                _store = Store[dict[str, float]](
+                    self.hass, 1, f"{DOMAIN}_storage_{device_info.deviceId}"
+                )
                 _sData = await _store.async_load()
                 try:
                     if _sData is not None:
-                        _sData =  {
-                            # use stored values if no user input, however user input always overwrites stored values
+                        _sData = {
                             BASE_PRODUCED_P1: float(user_input.get(BASE_PRODUCED_P1, _sData.get(BASE_PRODUCED_P1, 0))),
-                            BASE_PRODUCED_P2: float(user_input.get(BASE_PRODUCED_P2, _sData.get(BASE_PRODUCED_P2, 0)))
+                            BASE_PRODUCED_P2: float(user_input.get(BASE_PRODUCED_P2, _sData.get(BASE_PRODUCED_P2, 0))),
+                            DAILY_DEBOUNCE_P1: float(_sData.get(DAILY_DEBOUNCE_P1, 0)),
+                            DAILY_DEBOUNCE_P2: float(_sData.get(DAILY_DEBOUNCE_P2, 0)),
                         }
                     else:
-                        _sData =  {
+                        _sData = {
                             BASE_PRODUCED_P1: float(user_input.get(BASE_PRODUCED_P1, 0)),
-                            BASE_PRODUCED_P2: float(user_input.get(BASE_PRODUCED_P2, 0))
+                            BASE_PRODUCED_P2: float(user_input.get(BASE_PRODUCED_P2, 0)),
                         }
                     await _store.async_save(_sData)
                 except ValueError:
-                    _LOGGER.error("Value error while parsing base produced power. User input must be a float number in kWh.")
-                    errors[BASE_PRODUCED_P1] = "invalid value for base produced power. Must be a float number in kWh."
-                    errors[BASE_PRODUCED_P2] = "invalid value for base produced power. Must be a float number in kWh."
+                    _LOGGER.error(
+                        "Value error while parsing base produced power. User input must be a float number in kWh."
+                    )
+                    errors[BASE_PRODUCED_P1] = (
+                        "invalid value for base produced power. Must be a float number in kWh."
+                    )
+                    errors[BASE_PRODUCED_P2] = (
+                        "invalid value for base produced power. Must be a float number in kWh."
+                    )
                 else:
                     return self.async_update_reload_and_abort(
-                        self._get_reconfigure_entry(),
-                        data_updates=user_input
+                        self._get_reconfigure_entry(), data_updates=user_input
                     )
 
-        ip_configured = self._get_reconfigure_entry().data.get(CONF_IP_ADDRESS,"")
+        ip_configured = self._get_reconfigure_entry().data.get(CONF_IP_ADDRESS, "")
         port_configured = self._get_reconfigure_entry().data.get(CONF_PORT, DEFAULT_PORT)
         update_interval_configured = self._get_reconfigure_entry().data.get(UPDATE_INTERVAL, 15)
-        base_produced_p1_configured:float = self._get_reconfigure_entry().data.get(BASE_PRODUCED_P1, "")
-        base_produced_p2_configured:float = self._get_reconfigure_entry().data.get(BASE_PRODUCED_P2, "")
+        base_produced_p1_configured: float = self._get_reconfigure_entry().data.get(BASE_PRODUCED_P1, "")
+        base_produced_p2_configured: float = self._get_reconfigure_entry().data.get(BASE_PRODUCED_P2, "")
 
         session = async_get_clientsession(self.hass, False)
         api = APsystemsEZ1M(
@@ -159,21 +185,27 @@ class APsystemsLocalAPIFlow(ConfigFlow, domain=DOMAIN):
         _store = Store[dict[str, float]](self.hass, 1, f"{DOMAIN}_storage_{device_info.deviceId}")
         _sData = await _store.async_load()
         if _sData is not None:
-            base_produced_p1_configured=(_sData.get(BASE_PRODUCED_P1, base_produced_p1_configured))
-            base_produced_p2_configured=(_sData.get(BASE_PRODUCED_P2, base_produced_p2_configured))
+            base_produced_p1_configured = _sData.get(BASE_PRODUCED_P1, base_produced_p1_configured)
+            base_produced_p2_configured = _sData.get(BASE_PRODUCED_P2, base_produced_p2_configured)
 
         return self.async_show_form(
-            step_id="reconfigure",data_schema=
-                vol.Schema(
-                    {
-                        vol.Required(CONF_IP_ADDRESS, default=ip_configured): cv.string,
-                        vol.Optional(CONF_PORT, default=port_configured): cv.port,
-                        vol.Optional(UPDATE_INTERVAL, default=update_interval_configured): int,
-                        vol.Optional(BASE_PRODUCED_P1, default=base_produced_p1_configured): cv.string,   # due to a bug, we need to read here a string and convert later
-                        vol.Optional(BASE_PRODUCED_P2, default=base_produced_p2_configured): cv.string
- #                       vol.Optional(BASE_PRODUCED_P1, default=base_produced_p1_configured): vol.Coerce(float),   do not work either
- #                       vol.Optional(BASE_PRODUCED_P2, default=base_produced_p2_configured): vol.Coerce(float)
-                    }
-                ),
+            step_id="reconfigure",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_IP_ADDRESS, default=ip_configured): cv.string,
+                    vol.Optional(CONF_PORT, default=port_configured): cv.port,
+                    vol.Optional(
+                        UPDATE_INTERVAL, default=update_interval_configured
+                    ): int,
+                    vol.Optional(
+                        BASE_PRODUCED_P1, default=base_produced_p1_configured
+                    ): cv.string,  # due to a bug, we need to read here a string and convert later
+                    vol.Optional(
+                        BASE_PRODUCED_P2, default=base_produced_p2_configured
+                    ): cv.string,
+                    #                       vol.Optional(BASE_PRODUCED_P1, default=base_produced_p1_configured): vol.Coerce(float),   do not work either
+                    #                       vol.Optional(BASE_PRODUCED_P2, default=base_produced_p2_configured): vol.Coerce(float)
+                }
+            ),
             errors=errors,
         )
